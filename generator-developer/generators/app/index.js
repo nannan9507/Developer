251a563b4fa436299495996618f87e8a8d2d9e33
413508034@qq.com <413508034@qq.com>
2017-01-28 16:00:53 +0800

    删除无用的chalk modules

--- generator-developer/generators/app/index.js
+++ generator-developer/generators/app/index.js
@@ -1,11 +1,9 @@
 'use strict';
 var Generator = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
-module.exports = Generator.extend({
+module.exports = class extends Generator{
   initializing() {
     console.log(222)
     this.composeWith('developer:configure')
   }
-});
+};
